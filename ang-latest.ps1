be24c1176ab9455310e52bf4ca53365d694c803f
Julius Bacosa <juliusbacosa@gmail.com>
2021-02-23 10:46:23 +0800

    .

---
+++ ang-latest.ps1
@@ -0,0 +1,4 @@
+nvm use 15.9.0
+npm uninstall -g @angular/cli 
+npm cache clean --force
+npm install -g @angular/cli@latest
---
+++ cb-version.ps1
@@ -0,0 +1,4 @@
+nvm use 14.15.4
+npm uninstall -g @angular/cli 
+npm cache clean --force
+npm install -g @angular/cli@8.1.2
